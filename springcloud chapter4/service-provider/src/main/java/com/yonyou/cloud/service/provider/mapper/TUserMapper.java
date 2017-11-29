26943c94809dfc7b19e21b886572b0fec0dfdafd
liuxudong-yonyou <dong1987>
2017-11-29 11:34:00 +0800

    no message

--- springcloud chapter4/service-provider/src/main/java/com/yonyou/cloud/service/provider/mapper/TUserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.yonyou.cloud.service.provider.mapper;
-
-import com.yonyou.cloud.service.provider.entity.TUser;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface TUserMapper extends Mapper<TUser> {
-}
