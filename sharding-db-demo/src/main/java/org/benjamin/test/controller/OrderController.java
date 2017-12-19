680fbb425a3857c93bec1f6eae965d207733b26b
liuxudong-yonyou <dong1987>
2017-12-19 13:48:30 +0800

    测试事务

--- sharding-db-demo/src/main/java/org/benjamin/test/controller/OrderController.java
+++ sharding-db-demo/src/main/java/org/benjamin/test/controller/OrderController.java
@@ -1,18 +1,28 @@
 package org.benjamin.test.controller;
 
 import org.benjamin.test.entity.TOrder;
-import org.benjamin.test.entity.TUser;
 import org.benjamin.test.service.OrderService;
-import org.benjamin.test.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.yonyou.cloud.common.annotation.YcApi;
+import com.yonyou.cloud.common.beans.RestResultResponse;
 import com.yonyou.cloud.common.controller.BaseController;
 
 @RestController
 @RequestMapping("/order")
-@YcApi
 public class OrderController extends BaseController<OrderService,TOrder>{
+	
+	@Autowired
+	OrderService orderService;
 
+	@RequestMapping(value="/saveAndDel",method=RequestMethod.POST)
+	@YcApi
+	public RestResultResponse<String> saveAndDelOrder(@RequestBody TOrder order){
+		orderService.SaveNewOrder(order);
+		 return new RestResultResponse<String>().success(true);
+	}
 }
--- sharding-db-demo/src/main/java/org/benjamin/test/service/OrderService.java
+++ sharding-db-demo/src/main/java/org/benjamin/test/service/OrderService.java
@@ -2,13 +2,40 @@ package org.benjamin.test.service;
 
 import org.benjamin.test.entity.TOrder;
 import org.benjamin.test.entity.TUser;
+import org.benjamin.test.mapper.TOrderMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.yonyou.cloud.common.service.BaseService;
+import com.yonyou.cloud.common.service.utils.EntityUtils;
 
 import tk.mybatis.mapper.common.Mapper;
 
 @Service
-public class OrderService extends BaseService<Mapper<TOrder>, TOrder>{
+@Transactional
+public class OrderService extends BaseService<Mapper<TOrder>, TOrder> {
+	
+	@Autowired
+	UserService userService;
+
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	public void SaveNewOrder(TOrder entity) {
+		TOrder delOrder = new TOrder();
+		delOrder.setOrderNo(entity.getOrderNo());
+		mapper.delete(delOrder);
+
+		this.insertSelective(entity);
+		
+		TUser user = new TUser();
+		user.setName("匿名用户");
+		
+		userService.insertSelective(user);
+		
+		int i = 1/0;
+	}
 
 }
